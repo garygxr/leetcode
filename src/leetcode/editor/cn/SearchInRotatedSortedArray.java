e2485d905b032d17ac38a74810cbde4d5c2c5ddf
xinrui <ganxinrui123>
2022-01-07 12:07:16 +0800

    第3天

--- src/leetcode/editor/cn/SearchInRotatedSortedArray.java
+++ src/leetcode/editor/cn/SearchInRotatedSortedArray.java
@@ -20,7 +20,7 @@ package leetcode.editor.cn;
 // 示例 2： 
 //
 // 
-//输入：nums = [4,5,6,7,0,1,2], target = 3
+//输入：nums = [5,1,2,3,4], target = 3
 //输出：-1 
 //
 // 示例 3： 
@@ -47,18 +47,40 @@ package leetcode.editor.cn;
 // 进阶：你可以设计一个时间复杂度为 O(log n) 的解决方案吗？ 
 // Related Topics 数组 二分查找 👍 1762 👎 0
 
-public class SearchInRotatedSortedArray{
+public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
         Solution solution = new SearchInRotatedSortedArray().new Solution();
-        
+        int search = solution.search(new int[]{3, 4, 5, 1, 2}, 1);
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int search(int[] nums, int target) {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int search(int[] nums, int target) {
+            int n = nums.length;
+            int l = 0, r = n - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
 
+                if (nums[0] <= nums[mid]) {
+                    if (nums[0] <= target && target < nums[mid]) {
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] < target && target <=nums[n-1]){
+                        l = mid+1;
+                    }else {
+                        r = mid-1;
+                    }
+                }
+            }
+            return -1;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
